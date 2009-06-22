24bb131298e5af456f2004951a400569a7b19ae9
Tommy <tommy_k38@yahoo.com>
2009-06-22 13:18:45 -0400

    created a common assembly info class shared throughout the projects

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyVersionAttribute("0.1.0")]
+[assembly: AssemblyFileVersionAttribute("0.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.1.0")]
+//[assembly: AssemblyCopyrightAttribute("Copyright © Codai, LLC 2009")]
+[assembly: AssemblyCompanyAttribute("Tommy Kelly")]
+[assembly: AssemblyConfigurationAttribute("debug")]
+[assembly: AssemblyTrademarkAttribute("")]
+[assembly: AssemblyCultureAttribute("")]
+
--- SqlMigration.Runner/Properties/AssemblyInfo.cs
+++ SqlMigration.Runner/Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SqlMigration.Runner")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Belron US")]
-[assembly: AssemblyProduct("SqlMigration.Runner")]
-[assembly: AssemblyCopyright("Copyright © Belron US 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3717d98f-4119-43be-b4bb-19f8e801230b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("SqlMigration")]
