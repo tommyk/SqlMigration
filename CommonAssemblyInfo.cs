9b8352d821a3560e1c5543a15183e722bbc3f4e1
Tommy <tommy_k38@yahoo.com>
2009-06-23 10:33:43 -0400

    changed assembly info to start creating build numbers until release 0.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,12 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("0.1.0")]
-[assembly: AssemblyFileVersionAttribute("0.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.1.0")]
-//[assembly: AssemblyCopyrightAttribute("Copyright © Codai, LLC 2009")]
+[assembly: AssemblyVersionAttribute("0.1.*")]
 [assembly: AssemblyCompanyAttribute("Tommy Kelly")]
-[assembly: AssemblyConfigurationAttribute("debug")]
+//[assembly: AssemblyConfigurationAttribute("debug")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-
