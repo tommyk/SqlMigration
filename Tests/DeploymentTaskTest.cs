8f5e29bb5bb179194ebc6fc55a2f44dc5441e2aa
tommyk <tommy_k38@yahoo.com>
2011-03-16 16:47:56 -0400

    cleaned up sql so its ANSI. (there you go dave michels)

--- Tests/DeploymentTaskTest.cs
+++ Tests/DeploymentTaskTest.cs
@@ -61,7 +61,7 @@ set @debug = @debug + CHAR(13) + 'Starting 2008-01-01_01h11m-test.sql'
 exec ('command1')
 exec ('command2')
 set @debug = @debug + CHAR(13) + 'Ending 2008-01-01_01h11m-test.sql'
-INSERT INTO SqlMigration VALUES ('2008-01-01_01h11m-test.sql')
+INSERT INTO SqlMigration (Name) VALUES ('2008-01-01_01h11m-test.sql')
 END
 IF (SELECT COUNT(NAME) FROM SqlMigration WHERE Name = '2008-01-01_01h12m-test.sql') = 0
 BEGIN
@@ -69,7 +69,7 @@ set @debug = @debug + CHAR(13) + 'Starting 2008-01-01_01h12m-test.sql'
 exec ('command1')
 exec ('command2')
 set @debug = @debug + CHAR(13) + 'Ending 2008-01-01_01h12m-test.sql'
-INSERT INTO SqlMigration VALUES ('2008-01-01_01h12m-test.sql')
+INSERT INTO SqlMigration (Name) VALUES ('2008-01-01_01h12m-test.sql')
 END
 COMMIT TRANSACTION SqlMigrationTransaction
 SELECT @debug as Tracing
