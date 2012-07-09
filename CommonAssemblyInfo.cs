ce9fb16ad4e589729b4542bc556775b6d08ea310
tommyk <tommy_k38@yahoo.com>
2012-07-09 11:25:36 -0400

    Updating version number for build

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("0.1.1")]
+[assembly: AssemblyVersionAttribute("0.1.2")]
 [assembly: AssemblyCompanyAttribute("Tommy Kelly")]
 //[assembly: AssemblyConfigurationAttribute("debug")]
 [assembly: AssemblyTrademarkAttribute("")]
