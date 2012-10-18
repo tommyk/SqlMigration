3325eb6a7665fcc493c4f6b280cddf0de2359ca7
tommy kelly <tommy_k38@yahoo.com>
2012-10-18 11:44:22 -0400

    Updated version to 0.1.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("0.1.2")]
+[assembly: AssemblyVersionAttribute("0.1.3")]
 [assembly: AssemblyCompanyAttribute("Tommy Kelly")]
 //[assembly: AssemblyConfigurationAttribute("debug")]
 [assembly: AssemblyTrademarkAttribute("")]
