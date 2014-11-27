272a202e855c55e3b65305967a43c13d5a3eed88
Tommy Kelly <tommy_k38@yahoo.com>
2014-11-27 16:26:33 -0500

    added stuff to create nuget packages

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
+//update the nuget package file if you update this version
 [assembly: AssemblyVersionAttribute("0.1.4")]
 [assembly: AssemblyCompanyAttribute("Tommy Kelly")]
 //[assembly: AssemblyConfigurationAttribute("debug")]
